ef325e4f12ad042598cb52b7b4ffb298324d7dab
Mehetaj <121656128+Mehetaj@users.noreply.github.com>
2023-05-19 10:18:22 +0600

    toy details added

--- src/Pages/Home/Alltoy/ToysDetails.jsx
+++ src/Pages/Home/Alltoy/ToysDetails.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import { Rating } from '@smastrom/react-rating';
+import React, { useContext } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../../Authprovider/AuthProvider';
 
 const ToysDetails = () => {
+
+    // picture, toy name, seller name, seller email, price, rating, available quantity, and detail description
+
+    const toy = useLoaderData()
+    // console.log(toy);
+    const { picture, name, price,ratings,quantity,description } = toy;
+    const rating = parseInt(ratings)
+
+    const {user} = useContext(AuthContext)
+
     return (
         <div>
-            
+            <div className="card h-[800px] lg:card-side bg-base-100 shadow-xl">
+                <figure><img className='w-[800px] p-10 mt-10' src={picture} alt="Album" /></figure>
+                <div className="card-body mt-72">
+                    <h2 className="card-title text-3xl">{name}</h2>
+                    <div>
+                    <p>Available Quantity:  {quantity}</p>
+                    <p className='w-1/2 my-4'>{description}</p>
+
+                    <div className='mt-10'>
+                        <h2 className='text-3xl font-bold'>Seller Information</h2>
+                        <p className='text-xl mt-4'>Seller Name: {user?.displayName}</p>
+                        <p className='text-xl mt-4'>Seller Mail: {user?.email}</p>
+                    </div>
+                    </div>
+                    <div className="card-actions  justify-between mt-auto">
+                        <div className=''>
+                        <p className='mr-3'>Price: {price}</p>
+                        <p className='flex items-center'>Ratings: <Rating readOnly style={{width: '100px'}} value={rating}/></p>
+                        </div>
+                        <button className="btn btn-primary">Listen</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Shared/Footer.jsx
+++ src/Pages/Home/Shared/Footer.jsx
@@ -1,32 +1,37 @@
 import React from 'react';
+import logo from '../../../assets/the-word-of-toys-making-from-little-acrylic-cubes-in-black-background-2JDW7YR.jpg'
 
 const Footer = () => {
-    return (
-        <div className='mt-96'>
-            <footer className="py-8 shadow-xl p-10 rounded-lg">
-  <div className="container mx-auto px-4">
-    <div className="flex flex-wrap items-center justify-between">
-      <div className="w-full md:w-auto text-center md:text-left">
-        <p className=" text-lg font-bold">Your Company Name</p>
-        <p className="text-gray-400 text-sm">123 Street, City, Country</p>
-      </div>
-      <div className="w-full md:w-auto text-center md:text-right mt-4 md:mt-0">
-        <p className=" text-sm">Phone: (123) 456-7890</p>
-        <p className=" text-sm">Email: info@example.com</p>
-      </div>
-      <div className="w-full md:w-auto text-center">
-        <ul className="flex justify-center md:justify-end space-x-4">
-          <li><a href="#" className="text-gray-500 hover: text-sm">Home</a></li>
-          <li><a href="#" className="text-gray-500 hover: text-sm">About</a></li>
-          <li><a href="#" className="text-gray-500 hover: text-sm">Contact</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</footer>
-
+  return (
+    <div className='mt-96'>
+      <footer className="  footer p-10  text-base-content">
+        <div className='text-center lg:text-left'>
+          <img className='w-[100px] h-[100px] rounded-full' src={logo} alt="" />
+          <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
         </div>
-    );
+        <div>
+          <span className="footer-title">Services</span>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
