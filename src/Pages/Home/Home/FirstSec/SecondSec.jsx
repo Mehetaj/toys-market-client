d3d50e42c7171750e32768d197d4b37f3d09a896
Mehetaj <121656128+Mehetaj@users.noreply.github.com>
2023-05-20 18:15:22 +0600

    AOS pack added

--- src/Pages/Home/Home/FirstSec/SecondSec.jsx
+++ src/Pages/Home/Home/FirstSec/SecondSec.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import img from '../../../../assets/col.jpg'
+import Aos from 'aos';
+import 'aos/dist/aos.css'
 
 const SecondSec = () => {
+    useEffect(() => {
+        Aos.init({duration: 1000})
+    },[])
     return (
-        <div className='lg:flex justify-evenly my-20'>
-            <img className='w-[400px]' src={img} alt="" />
-            <div>
+        <div  className='lg:flex justify-evenly my-20'>
+            <img data-aos="slide-up" className='w-[400px]' src={img} alt="" />
+            <div data-aos="slide-up">
                 <h1 className='text-3xl font-bold font-serif'>This is Our Awesome Collection For You</h1>
                 <p className='w-[500px] my-4 text-gray-400'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea id laboriosam adipisci nulla at. Vero tenetur assumenda animi similique eius molestiae dolorem, sequi quis non provident! Nobis mollitia suscipit sapiente pariatur cumque facilis animi explicabo, praesentium qui dolores eius quia!.mollitia suscipit sapiente pariatur cumque facilis animi explicabo, praesentium qui dolores eius quia!</p>
             </div>
--- src/Pages/Home/Home/Gallery/Galary.jsx
+++ src/Pages/Home/Home/Gallery/Galary.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import img1 from '../../../../assets/toys/bus.jpg'
 import img2 from '../../../../assets/toys/fire.jpg'
 import img3 from '../../../../assets/toys/police.jpg'
 import img4 from '../../../../assets/toys/regular.webp'
 import img5 from '../../../../assets/toys/sports.jpg'
 import img6 from '../../../../assets/toys/truck.jpg'
+import Aos from 'aos';
+import 'aos/dist/aos.css'
 
 const Galary = () => {
+    useEffect(() => {
+        Aos.init({
+            duration: 1200,
+          })
+    },[])
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-            <img className='rounded-lg h-[350px] m-4  lg:p-1 bg-gray-100 w-full lg:w-[350px]'  src={img1} alt="" />
-            <img className='rounded-lg h-[350px] m-4  lg:p-1 bg-gray-100 w-full lg:w-[350px]'  src={img2} alt="" />
-            <img className='rounded-lg h-[350px] m-4  lg:p-1 bg-gray-100 w-full lg:w-[350px]'  src={img3} alt="" />
-            <img className='rounded-lg h-[350px] m-4  lg:p-1 bg-gray-100 w-full lg:w-[350px]'  src={img4} alt="" />
-            <img className='rounded-lg h-[350px] m-4  lg:p-1 bg-gray-100 w-full lg:w-[350px]'  src={img5} alt="" />
-            <img className='rounded-lg h-[350px] m-4  lg:p-1 bg-gray-100 w-full lg:w-[350px]'  src={img6} alt="" />
+            <div data-aos="zoom-in" className='item'><img  className='rounded-lg h-[350px] m-4  lg:p-1 bg-gray-100 w-full lg:w-[350px]'  src={img1} alt="" /></div>
+            <img data-aos="zoom-in" className='rounded-lg item h-[350px] m-4  lg:p-1 bg-gray-100 w-full lg:w-[350px]'  src={img2} alt="" />
+            <img data-aos="zoom-in" className='rounded-lg item h-[350px] m-4  lg:p-1 bg-gray-100 w-full lg:w-[350px]'  src={img3} alt="" />
+            <img data-aos="zoom-in" className='rounded-lg item h-[350px] m-4  lg:p-1 bg-gray-100 w-full lg:w-[350px]'  src={img4} alt="" />
+            <img data-aos="zoom-in" className='rounded-lg item h-[350px] m-4  lg:p-1 bg-gray-100 w-full lg:w-[350px]'  src={img5} alt="" />
+            <img data-aos="zoom-in" className='rounded-lg item h-[350px] m-4  lg:p-1 bg-gray-100 w-full lg:w-[350px]'  src={img6} alt="" />
         </div>
     );
 };
